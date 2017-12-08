16caefdae0856d4f83cf3bc1b1ed1eccbb8e4f12
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2017-12-08 20:47:50 -0300

    Add Project model

--- angular2cloud/dashboard/models.py
+++ angular2cloud/dashboard/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 # Create your models here.
+class Project(models.Model):
+    APP_STATUS = (
+        ('RN', 'Running'),
+        ('ST', 'Stopped'),
+    )
+    name = models.CharField(max_length = 120)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    domain = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=APP_STATUS,
+        default='RN',
+    )
+    source = models.FileField(upload_to=user_directory_path)
