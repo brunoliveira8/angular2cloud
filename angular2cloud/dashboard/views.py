af34694da2318642fc377a3a05662b35f915d832
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2017-12-09 13:25:51 -0300

    Block to create more than 2 projects

--- angular2cloud/dashboard/views.py
+++ angular2cloud/dashboard/views.py
@@ -12,5 +12,9 @@ class ProjectCreate(CreateView):
     success_url = "/"
 
     def form_valid(self, form):
+        n_projects = len(Project.objects.filter(user=self.request.user))
+        if n_projects == 2:
+            form.add_error(None, "You can create at most 2 projects")
+            return super().form_invalid(form)
         form.instance.user = self.request.user
         return super().form_valid(form)
