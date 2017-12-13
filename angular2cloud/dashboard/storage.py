18fb889247cce1335a23261f9bf2b70e7976c5ee
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2017-12-13 16:20:42 -0300

    Add storage that overwrites the file

---
+++ angular2cloud/dashboard/storage.py
@@ -0,0 +1,17 @@
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
+
+class OverwriteStorage(FileSystemStorage):
+
+    def get_available_name(self, name, max_length):
+        """Returns a filename that's free on the target storage system, and
+        available for new content to be written to.
+
+        Found at http://djangosnippets.org/snippets/976/
+
+        """
+        # If the filename already exists, remove it as if it was a true file system
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return super(OverwriteStorage, self).get_available_name(name, max_length)
